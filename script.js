4a278195391743212debb88fc0dca1b667344c44
Daniel13s <dansilvac254@gmail.com>
2024-10-30 21:28:25 -0300

    adição de interação em JS

---
+++ script.js
@@ -0,0 +1,15 @@
+function clicar1() {
+    window.scrollTo(0, 800)
+}
+function clicar2() {
+    window.scrollTo(0, 800)
+}
+function clicar3() {
+    window.scrollTo(0, 1400)
+}
+function clicar4() {
+    window.scrollTo(0, 1300)
+}
+function clicar5() {
+    window.scrollTo(0, 1900)
+}
