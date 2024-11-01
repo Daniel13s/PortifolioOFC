5fc49329c6085689fa6dc420fd926489038390ae
Daniel Silva <165176862+Daniel13s@users.noreply.github.com>
2024-11-01 11:30:55 -0300

    Adicionando navegação fixa

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function clicar1() {
-    window.scrollTo(0, 800)
+    window.scrollTo(0, 650)
 }
 function clicar2() {
-    window.scrollTo(0, 800)
+    window.scrollTo(0, 650)
 }
 function clicar3() {
-    window.scrollTo(0, 1400)
+    window.scrollTo(0, 1200)
 }
 function clicar4() {
-    window.scrollTo(0, 1300)
+    window.scrollTo(0, 1200)
 }
 function clicar5() {
-    window.scrollTo(0, 1900)
+    window.scrollTo(0, 1750)
 }
